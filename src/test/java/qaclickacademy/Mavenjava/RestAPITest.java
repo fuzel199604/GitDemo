ff66542ff9481052ea5c5aa2af575c4a554ad196
fuzel <fazilshaik.md24@gmail.com>
2021-02-24 15:41:42 +0530

    this is first git commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	
+	@Test
+	public void PostJIRA(){
+		System.out.println("POstJIRA");
+	}
+	
+	
+	@Test
+	public void deleteTwitter(){
+		System.out.println("deleteTwitter");
+	}
+
+}
